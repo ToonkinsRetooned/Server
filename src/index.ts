f6c3e6a6c94fab642c5b84596eaa7a633f20deef
Index <xv.media.address@gmail.com>
2024-11-05 14:26:36 -0600

    feat: propagate "startPlayingMinigame" websocket event

--- src/index.ts
+++ src/index.ts
@@ -342,9 +342,19 @@ const app = new Elysia()
             playerId: player.id
           });
           break
+        case 'startPlayingMinigame':
+          propagateEvent(function (player: SerializedPlayer, sender: SerializedPlayer) {
+            return player.roomId == sender.roomId
+          }, players[ticket], {
+            type: 'startPlayingMinigame',
+            playerId: player.id
+          });
+          break
       };
     },
 
+    // ! for some reason the closing of the websocket isn't fired when the tab is closed
+    // TODO: kick the player for inactivity if they haven't sent a heartbeat packet in awhile
     close(ws) {
       const { ticket } = ws.data.query as { ticket: string };
 
